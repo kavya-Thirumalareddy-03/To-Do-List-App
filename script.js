85d148977b3f52d073806cf75bda5742f5fbced1
kavya-Thirumalareddy-03 <140062307+kavya-Thirumalareddy-03@users.noreply.github.com>
2025-03-11 18:35:34 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+function addTask() {
+    let taskInput = document.getElementById("taskInput");
+    let taskList = document.getElementById("taskList");
+
+    if (taskInput.value.trim() !== "") {
+        let li = document.createElement("li");
+        li.textContent = taskInput.value;
+
+        let deleteBtn = document.createElement("button");
+        deleteBtn.textContent = "❌";
+        deleteBtn.onclick = function () {
+            li.remove();
+        };
+
+        li.appendChild(deleteBtn);
+        taskList.appendChild(li);
+        taskInput.value = "";
+    }
+}
